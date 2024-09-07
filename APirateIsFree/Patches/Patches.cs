fd761ff565f945c26aedebfc627fcb0158890e0e
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-09-07 18:17:11 -0400

    rizz

--- APirateIsFree/Patches/Patches.cs
+++ APirateIsFree/Patches/Patches.cs
@@ -13,7 +13,7 @@ namespace APirateIsFree.Patches
         private static bool Prefix(ref VRRig __instance, ref PhotonMessageInfo info)
         {
             __instance.IncrementRPC(info, "RequestCosmetics");
-            if (__instance.photonView.IsMine && CosmeticsController.hasInstance)
+            if (__instance.netView.IsMine && CosmeticsController.hasInstance)
             {
                 string[] array = CosmeticsController.instance.currentWornSet.ToDisplayNameArray();
                 string[] array2 = CosmeticsController.instance.tryOnSet.ToDisplayNameArray();
@@ -31,7 +31,7 @@ namespace APirateIsFree.Patches
                 }
                 var newarr = modifiedList.ToArray();
                 Debug.Log(newarr.ToJson());
-                __instance.photonView.RPC("UpdateCosmeticsWithTryon", info.Sender, newarr, array2);
+                __instance.netView.SendRPC("UpdateCosmeticsWithTryon", info.Sender, newarr, array2);
             }
 
             return false;
@@ -68,6 +68,41 @@ namespace APirateIsFree.Patches
                 CosmeticsController.instance.UpdateWornCosmetics();
                 CosmeticsController.instance.UpdateMyCosmetics();
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(CosmeticsController))]
+    [HarmonyPatch(nameof(CosmeticsController.CompareCategoryToSavedCosmeticSlots))]
+    internal class CheckPatch
+    {
+        private static bool Prefix(CosmeticsController __instance, ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CosmeticsController.CosmeticSet))]
+    [HarmonyPatch(nameof(CosmeticsController.CosmeticSet.LoadFromPlayerPreferences))]
+    internal class SlotApplyPatch
+    {
+        private static bool Prefix(CosmeticsController.CosmeticSet __instance, CosmeticsController controller)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                CosmeticsController.CosmeticSlots cosmeticSlots = (CosmeticsController.CosmeticSlots)i;
+                string @string = PlayerPrefs.GetString(CosmeticsController.CosmeticSet.SlotPlayerPreferenceName(cosmeticSlots), "NOTHING");
+                if (@string == "null" || @string == "NOTHING")
+                {
+                    __instance.items[i] = controller.nullItem;
+                }
+                else
+                {
+                    CosmeticsController.CosmeticItem item = controller.GetItemFromDict(@string);
+                    __instance.items[i] = item;
+                }
+            }
+            return false;
         }
     }
 }
